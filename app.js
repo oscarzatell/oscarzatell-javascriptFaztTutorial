5d16daae45673f49816fb041288f6f3c763641c2
Oscar Zamora Tellez <oscarnewsletter1@gmail.com>
2020-04-30 00:36:25 +0000

    tipos de datos

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-document.write("<h1>hello world</h1>");
+// tipos de datos
+"Hello world" // string
+'Hello world' // string
+
+1000000     // number
+-2.3        // number
+
+//boolean
+true
+false
+
+// array
+['joe', 'ryan', 'martha']
+[1, 2, 3]
+[true, false, true, false]
+
+// object
+{
+    "username": 'ryan',
+    "score": 70.4,
+    "hours": 14,
+    "proffesional": true,
+    "friends": ['', '', '']
+
+}
